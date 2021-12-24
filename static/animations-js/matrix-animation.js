e3df59fdbaa267dbce06ddc971e49ecc6350bd4a
MagnoGG <marcosgranja317@gmail.com>
2021-12-24 01:04:32 +0100

    Added margins and identation to the animatino

--- static/animations-js/matrix-animation.js
+++ static/animations-js/matrix-animation.js
@@ -1,63 +1,79 @@
-const desiredFPS = 20;
+// Config values
+// -------------
+const clearColor = "#00000005";
+const fontColor = ["#f11", "#0f0"];
+const fontSize = 20;
 
-const clearColor = "#0001"; // "#21212130";
-const fontColor = "#f00d";
-const fontSize = 30;
+const marginLeft = 50;
+const marginRight = 100;
+const ident = fontSize * 4;
 
 const firstChar = 913;
 const lastChar = 1023;
 const numberChars = lastChar - firstChar;
 
 (() => {
-const canvas = document.getElementById("matrix-animation");
-const ctx = canvas.getContext("2d");
+    const canvas = document.getElementById("matrix-animation");
+    const ctx = canvas.getContext("2d");
 
-let counter = 0;
-let lastTime = 0;
+    let counter = 0;
+    let lastTime = 0;
 
-addEventListener("resize", resize);
-function resize() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    if (window.innerHeight < 400) {
-        canvas.height = 400;
-    }
-}
-
-function loop(currentTime) {
-    const deltaTime = currentTime - lastTime;
-    const fps = 1000 / deltaTime;
-
-    const desiredDelta = 1000 / desiredFPS;
-
-    if (desiredDelta <= deltaTime) {
-        render();
-        lastTime = currentTime;
+    addEventListener("resize", resize);
+    function resize() {
+        // Update the canvas exactly as the CSS
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        if (window.innerHeight < 400) {
+            canvas.height = 400;
+        }
     }
 
-    requestAnimationFrame(loop);
-}
+    function loop(currentTime) {
+        const deltaTime = currentTime - lastTime;
+        const fps = 1000 / deltaTime;
 
-function render() {
-    ctx.fillStyle = clearColor;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+        const desiredFPS = Math.floor(Math.random() * 20);
+        const desiredDelta = 1000 / desiredFPS;
 
-    const totalCols = Math.floor(canvas.width / fontSize);
-    const totalRows = Math.floor(canvas.height / fontSize);
+        // Fade out effect with a color with alpha
+        ctx.fillStyle = clearColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    const currentRow = ++counter % totalRows;
-    const colsOffset = Math.floor(Math.random() * totalCols);
+        // Don't render or update until the desired delta has passed
+        if (desiredDelta <= deltaTime) {
+            render();
+            lastTime = currentTime;
+        }
 
-    for (let i = 0; i <= totalCols; i++) {
-        const randomChar = String.fromCharCode(Math.random() * numberChars + firstChar);
-
-        ctx.fillStyle = fontColor;
-        ctx.font = `${fontSize}px Consolas`;
-        // TODO: Improve the performance by avoiding rendering chars out of the view port
-        // TODO: Use different colors, rhythms, patterns, etc
-        ctx.fillText(randomChar, (i-colsOffset)*fontSize, (currentRow+1)*fontSize);
+        requestAnimationFrame(loop);
     }
-}
-resize();
-loop();
+
+    // TODO: Use different patterns, etc
+    let identlevel = 0;
+    function render() {
+        const random = Math.random();
+
+        if (identlevel < 3 && random > 0.8)
+        identlevel++;
+        if (identlevel > 0 && random < 0.2)
+        identlevel--;
+
+        const totalCols = Math.floor((canvas.width - marginLeft - marginRight - ident*identlevel) / fontSize);
+        const totalRows = Math.floor(canvas.height / fontSize);
+
+        const currentRow = ++counter % totalRows+1;
+        const colsOffset = Math.floor(Math.random() * totalCols);
+
+        for (let i = 0; i <= colsOffset; i++) {
+            const randomChar = String.fromCharCode(Math.floor(Math.random() * numberChars + firstChar));
+
+            ctx.fillStyle = fontColor[Math.floor(Math.random() * fontColor.length)];
+            ctx.font = `${fontSize}px Consolas`;
+            ctx.fillText(randomChar, i*fontSize+marginLeft+ident*identlevel, currentRow*fontSize);
+        }
+    }
+
+    resize();
+    loop();
 })();
