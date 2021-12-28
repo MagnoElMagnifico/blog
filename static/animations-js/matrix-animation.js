18d771b6ccce4c31537d65b187207b702c0ac946
MagnoGG <marcosgranja317@gmail.com>
2021-12-28 01:13:57 +0100

    Extracted random generation into funtion and now some config values change when reloading

--- static/animations-js/matrix-animation.js
+++ static/animations-js/matrix-animation.js
@@ -1,24 +1,45 @@
+// Returns a random integer in the range [start, end)
+function random(start, end) {
+    return Math.floor((Math.random() * (end - start) + start));
+}
+
+// Returns a random bool
+function random_bool() {
+    return random(0, 2) == 0;
+}
+
 // Config values
 // -------------
 const clearColor = "#00000007";
-const fontColor = ["#0f0"];//["#f00", "#0f0"];
+const possibleFontColor = ["#f00", "#0f0"];
 const fontSize = 20;
 
 const marginLeft = 50;
 const marginRight = 100;
 const ident = fontSize * 4;
 
-// UTF-16BE - Normal - Japanese - Greek
-const firstChar = 48;// 12032; // 913;
-const lastChar = 112;// 12245;  // 1023;
-const numberChars = lastChar - firstChar;
+let firstChar;
+let lastChar;
+
+// UTF-16BE
+switch(random(0, 3)) {
+    case 0: firstChar = 48;    lastChar = 112;   break; // Normal
+    case 1: firstChar = 12032; lastChar = 12245; break; // Japanese
+    case 2: firstChar = 913;   lastChar = 1023;  break; // Greek
+    default: console.error("Unreachable");
+}
+
+let fontColor = [possibleFontColor[random(0, possibleFontColor.length)]];
+if (random_bool()) {
+    fontColor.push(possibleFontColor[random(0, possibleFontColor.length)]);
+}
+if (random_bool()) {
+    fontColor.push(possibleFontColor[random(0, possibleFontColor.length)]);
+}
 
 (() => {
     const canvas = document.getElementById("matrix-animation");
     const ctx = canvas.getContext("2d");
-
-    let counter = 0;
-    let lastTime = 0;
 
     addEventListener("resize", resize);
     function resize() {
@@ -34,6 +55,7 @@ const numberChars = lastChar - firstChar;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
+    let lastTime = 0;
     function loop(currentTime) {
         const deltaTime = currentTime - lastTime;
         const fps = 1000 / deltaTime;
@@ -55,25 +77,25 @@ const numberChars = lastChar - firstChar;
     }
 
     // TODO: Use different patterns, etc
+    let counter = -10;
     let identlevel = 0;
     function render() {
-        const random = Math.random();
-
-        if (identlevel < 3 && random > 0.8)
-        identlevel++;
-        if (identlevel > 0 && random < 0.2)
-        identlevel--;
+        switch (random(0, 10)) {
+            case 1: case 2: if (identlevel < 3) identlevel++; break;
+            case 8: case 9: if (identlevel > 0) identlevel--; break;
+            default: break;
+        }
 
         const totalCols = Math.floor((canvas.width - marginLeft - marginRight - ident*identlevel) / fontSize);
         const totalRows = Math.floor(canvas.height / fontSize);
 
         const currentRow = ++counter % totalRows+1;
-        const colsOffset = Math.floor(Math.random() * totalCols);
+        const colsOffset = random(0, totalCols);
 
         for (let i = 0; i <= colsOffset; i++) {
-            const randomChar = String.fromCharCode(Math.floor(Math.random() * numberChars + firstChar));
+            const randomChar = String.fromCharCode(random(firstChar, lastChar));
 
-            ctx.fillStyle = fontColor[Math.floor(Math.random() * fontColor.length)];
+            ctx.fillStyle = fontColor[random(0, fontColor.length)];
             ctx.font = `${fontSize}px Consolas`;
             ctx.fillText(randomChar, i*fontSize+marginLeft+ident*identlevel, currentRow*fontSize);
         }
