d3b9840dc9a3550f59061d5a452ef78ec188ec4b
MagnoElMagnifico <marcosgranja317@gmail.com>
2022-09-03 22:41:59 +0200

    Refactorizar código de la animación

--- static/js/matrix-animation.js
+++ static/js/matrix-animation.js
@@ -1,17 +1,10 @@
-// Returns a random integer in the range [start, end)
-function random(start, end) {
-    return Math.floor((Math.random() * (end - start) + start));
-}
-
-// Returns a random bool
-function random_bool() {
-    return random(0, 2) == 0;
-}
-
 // Config values
 // -------------
+const backColor = "#000";
 const clearColor = "#00000007";
 const possibleFontColor = ["#f00", "#0f0"];
+
+const fontFamily = "Fira Code";
 const fontSize = 20;
 
 const marginLeft = 50;
@@ -21,27 +14,31 @@ const ident = fontSize * 4;
 let firstChar;
 let lastChar;
 
-// UTF-16BE
-switch(random(0, 3)) {
+// Select characters. UTF-16BE codes
+switch(0) {//switch(random(0, 3)) {
     case 0: firstChar = 48;    lastChar = 112;   break; // Normal
     case 1: firstChar = 12032; lastChar = 12245; break; // Japanese
     case 2: firstChar = 913;   lastChar = 1023;  break; // Greek
     default: console.error("Unreachable");
 }
 
-let fontColor = [possibleFontColor[random(0, possibleFontColor.length)]];
-if (random_bool()) {
-    fontColor.push(possibleFontColor[random(0, possibleFontColor.length)]);
-}
-if (random_bool()) {
-    fontColor.push(possibleFontColor[random(0, possibleFontColor.length)]);
-}
-
 (() => {
-    const canvas = document.getElementById("matrix-animation");
-    const ctx = canvas.getContext("2d");
 
-    addEventListener("resize", resize);
+    class Animation {
+        constructor() {
+        }
+    }
+
+    // Returns a random integer in the range [start, end)
+    function random(start, end) {
+        return Math.floor((Math.random() * (end - start) + start));
+    }
+
+    // Returns a random bool
+    function random_bool() {
+        return random(0, 2) == 0;
+    }
+
     function resize() {
         // Update the canvas exactly as the CSS
         canvas.width = window.innerWidth;
@@ -49,9 +46,10 @@ if (random_bool()) {
         if (window.innerHeight < 400) {
             canvas.height = 400;
         }
+
         // Render a background to avoid seeing the background of the page
         // and not the canvas one
-        ctx.fillStyle = "black";
+        ctx.fillStyle = backColor;
         ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 
@@ -60,8 +58,9 @@ if (random_bool()) {
         const deltaTime = currentTime - lastTime;
         const fps = 1000 / deltaTime;
 
-        const desiredFPS = Math.floor(Math.random() * 20);
+        const desiredFPS = random(1, 5);
         const desiredDelta = 1000 / desiredFPS;
+        console.log(desiredFPS);
 
         // Fade out effect with a color with alpha
         ctx.fillStyle = clearColor;
@@ -76,9 +75,12 @@ if (random_bool()) {
         requestAnimationFrame(loop);
     }
 
-    // TODO: Use different patterns, etc
-    let counter = -10;
+    let counter = 0;
     let identlevel = 0;
+
+    const totalCols = Math.floor((canvas.width - marginLeft - marginRight - ident*identlevel) / fontSize);
+    const totalRows = Math.floor(canvas.height / fontSize);
+
     function render() {
         switch (random(0, 10)) {
             case 1: case 2: if (identlevel < 3) identlevel++; break;
@@ -86,21 +88,31 @@ if (random_bool()) {
             default: break;
         }
 
-        const totalCols = Math.floor((canvas.width - marginLeft - marginRight - ident*identlevel) / fontSize);
-        const totalRows = Math.floor(canvas.height / fontSize);
-
         const currentRow = ++counter % totalRows+1;
         const colsOffset = random(0, totalCols);
 
         for (let i = 0; i <= colsOffset; i++) {
             const randomChar = String.fromCharCode(random(firstChar, lastChar));
-
             ctx.fillStyle = fontColor[random(0, fontColor.length)];
-            ctx.font = `${fontSize}px Consolas`;
+            ctx.font = `${fontSize}px ${fontFamily}`;
+
             ctx.fillText(randomChar, i*fontSize+marginLeft+ident*identlevel, currentRow*fontSize);
         }
     }
 
+    // Init
+    // -------------------
+
+    // Select up to 3 diferent colors
+    let fontColor = [];
+    for (let i = random(1, 4); i > 0; i--) {
+        fontColor.push(possibleFontColor[random(0, possibleFontColor.length)]);
+    }
+
+    const canvas = document.getElementById("matrix-animation");
+    const ctx = canvas.getContext("2d");
+
+    addEventListener("resize", resize);
     resize();
     loop();
 })();
